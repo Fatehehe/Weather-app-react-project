8d4c3dfff6876f2b101082aacc8cf614bfd3ae13
Fate <fatakhillah.khaqo@gmail.com>
2023-10-03 13:38:36 +0700

    commiting finished project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import hotBg from "./assets/hot.jpg";
+import coldBg from "./assets/cold.jpg";
+import Description from "./components/Description";
+import { useEffect, useState } from "react";
+import getFormattedWeatherData from "./weatherService";
 
 function App() {
+  const [city, setCity] = useState("Jakarta");
+  const [weather, setWeather] = useState(null);
+  const [units, setUnits] = useState("metric");
+  const [bg, setBG] = useState(hotBg);
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      const data = await getFormattedWeatherData(city, units);
+      setWeather(data);
+
+      //dynamic bg
+      const threshold = units === "metric" ? 20 : 60;
+      if (data.temp <= threshold) {
+        setBG(coldBg);
+      } else {
+        setBG(hotBg);
+      }
+    };
+
+    fetchWeatherData();
+  }, [units, city]);
+
+  const handleUnitsClick = (e) => {
+    const button = e.currentTarget;
+    const currentUnit = button.innerText;
+
+    const isCelcius = currentUnit === "C";
+    button.innerText = isCelcius ? "F" : "C";
+    setUnits(isCelcius ? "metric" : "imperial");
+  };
+
+  const enterKeyPressed = (e) => {
+    if (e.keyCode === 13) {
+      setCity(e.currentTarget.value);
+      // e.currentTarget.blur()
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app" style={{ backgroundImage: `url(${bg})` }}>
+      <div className="overlay">
+        {weather && (
+          <div className="container">
+            <div className="section section__inputs">
+              <input
+                onKeyDown={enterKeyPressed}
+                type="text"
+                name="city"
+                placeholder="Enter City..."
+              />
+              <button onClick={handleUnitsClick}>F</button>
+            </div>
+
+            <div className="section section__temperature">
+              <div className="icon">
+                <h3>{`${weather.name}, ${weather.country}`}</h3>
+                <img src={weather.iconURL} alt="weatherIcon" />
+                <h3>{weather.description}</h3>
+              </div>
+              <div className="temperature">
+                <h1>{`${weather.temp.toFixed()}${
+                  units === "metric" ? "C" : "F"
+                }`}</h1>
+              </div>
+            </div>
+
+            {/* botton description */}
+            <Description weather={weather} units={units} />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
